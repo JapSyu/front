66941a92c7a8455ff947326201e8b87490f3f0aa
jeonjunbae <wnsqowid0901@gmail.com>
2025-08-23 22:54:19 +0900

    chore: 빌드시 ESLint 무시 설정

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 시 ESLint 무시
+  },
+}
+
 export default nextConfig;
